099652e5d85cf3379ed5434b6e1fa2f5f2208614
Nana Janashia <nn_janashia@yahoo.com>
2022-03-27 13:58:16 +0000

    Update server.test.js

--- app/server.test.js
+++ app/server.test.js
@@ -3,5 +3,5 @@ const fs = require('fs')
 
 test('main index.html file exists', () => {
   const filePath = path.join(__dirname, "index.html")
-  expect(fs.existsSync(filePath)).toBeFalsy();
+  expect(fs.existsSync(filePath)).toBeTruthy();
 });
