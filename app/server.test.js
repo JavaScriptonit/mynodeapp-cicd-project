682670552c4e0c88cc77134444a674aa8bdffb5d
Nana Janashia <nn_janashia@yahoo.com>
2022-04-13 10:04:53 +0000

    Update server.test.js

--- app/server.test.js
+++ app/server.test.js
@@ -13,5 +13,5 @@ test('Dockerfile exists', () => {
 
 test('.gitignore file exists', () => {
   const filePath = path.join(__dirname, "..", ".gitignore")
-  expect(fs.existsSync(filePath)).toBeFalsy();
+  expect(fs.existsSync(filePath)).toBeTruthy();
 });
