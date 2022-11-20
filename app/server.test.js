7b5ba4e63bbdd8d56c36944dec6c08bd4adc0f48
Andrey Shabunov <shabunovaa@yandex.ru>
2022-11-20 17:04:04 +0500

    20.11 test sync commit update package-lock.json and package.json comments in server.test.js

--- app/server.test.js
+++ app/server.test.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const fs = require('fs')
 
+/*
+Tests to check the files exists: index.html, Dockerfile, .gitignore
+Generating logs file: junit.xml
+ */
 test('main index.html file exists', () => {
   const filePath = path.join(__dirname, "index.html")
   expect(fs.existsSync(filePath)).toBeTruthy();
